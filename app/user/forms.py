ae28aeb16914f532663660c9aeaebc44990106d2
Jeff Straney <straneyjeff@gmail.com>
2018-04-04 20:49:15 -0400

    revised user forms

--- app/user/forms.py
+++ app/user/forms.py
@@ -6,6 +6,8 @@ import wtforms.fields.html5 as html5
 
 from wtforms.validators import Required, Regexp, Length, Email, EqualTo, Optional
 
+from app.customized.wtforms import field_to_lower, field_to_title
+
 class EditUserForm (Form):
 
     id = HiddenField("User ID")
@@ -14,29 +16,33 @@ class EditUserForm (Form):
     first_name = TextField('First Name', [
         Required(message="You must provide your first name"),
         Length(min=1, max=45, message="First name must be between 1 and 45 characters")
-        ])
+        ],
+        filters=[field_to_title])
 
     last_name = TextField('Last Name', [
         Required(message="You must provide your last name"),
         Length(min=1, max=45, message="Last name must be between 1 and 45 characters")
-        ])
+        ],
+        filters=[field_to_title])
 
     username = TextField('Username', [
         Required(message="You must provide a username"),
         Length(min=1, max=45, message="Username must be between 1 and 45 characters")
-        ])
+        ],
+        filters=[field_to_lower])
 
     email = html5.EmailField('Email', [
         Required(message="You must provide an email address"),
         Length(min=1, max=60, message="Email must be between 1 and 60 characters")
-        ])
+        ],
+        filters=[field_to_lower])
 
     bitcoin_wallet_address = TextField("BitCoin Wallet Address", [
         Optional(),
         Regexp(r"^$|[13][a-km-zA-HJ-NP-Z1-9]{25,34}$")
         ])
 
-    pay_pall_email = html5.EmailField("PayPal Email Address", [
+    pay_pal_email = html5.EmailField("PayPal Email Address", [
         Optional(),
         ])
 
